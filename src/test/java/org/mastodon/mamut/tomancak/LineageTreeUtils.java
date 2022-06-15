e7858c7fdc2435546577b9693b1935bb840ad7e3
Matthias Arzt <arzt@mpi-cbg.de>
2022-06-15 17:09:29 +0200

    Implement an algorithm for tree matchen
    * An transformation between the two dataset is estimated from the labeled
      spots in the first time point of the two datasets
    * The tree is sorted such that the spindle direction matches between
      the two datasets. Scalar product > 0.
    * Currently it doesn't work. I can see in the track scheme that the trees
      don't match.

---
+++ src/test/java/org/mastodon/mamut/tomancak/LineageTreeUtils.java
@@ -0,0 +1,43 @@
+package org.mastodon.mamut.tomancak;
+
+import org.mastodon.collection.ObjectRefMap;
+import org.mastodon.collection.RefSet;
+import org.mastodon.collection.ref.ObjectRefHashMap;
+import org.mastodon.collection.ref.RefSetImp;
+import org.mastodon.mamut.model.ModelGraph;
+import org.mastodon.mamut.model.Spot;
+import org.mastodon.pool.PoolCollectionWrapper;
+
+import java.util.Set;
+
+public class LineageTreeUtils
+{
+	public static RefSet<Spot> getRoots( ModelGraph graph ) {
+		PoolCollectionWrapper<Spot> vertices = graph.vertices();
+		RefSetImp<Spot> roots = new RefSetImp<>( vertices.getRefPool() );
+		for( Spot spot : vertices )
+			if(spot.incomingEdges().isEmpty())
+				roots.add(spot);
+		return roots;
+	}
+
+	public static ObjectRefMap<String, Spot> getRootsMap( ModelGraph graph )
+	{
+		PoolCollectionWrapper<Spot> vertices = graph.vertices();
+		ObjectRefMap<String, Spot> roots = new ObjectRefHashMap<>( vertices.getRefPool() );
+		for( Spot spot : vertices )
+			if(spot.incomingEdges().isEmpty())
+				roots.put(spot.getLabel(), spot);
+		return roots;
+	}
+
+	public static Spot getDividingSpot( ModelGraph graph, final Spot spot )
+	{
+		Spot s = graph.vertexRef();
+		s.refTo( spot );
+		while(s.outgoingEdges().size() == 1) {
+			s = s.outgoingEdges().get( 0 ).getTarget(s);
+		}
+		return s;
+	}
+}
